9d63e48eeba356f568ec5823e29fb598688eb515
Cristian Augusto Carneiro <cristian.carneiro@zema.com>
2020-07-28 10:54:18 -0300

    ListProviderDayAvailabilityService.spec.ts

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 6, 30, 8, 0, 0),
+      date: new Date(2020, 6, 30, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 6, 30, 10, 0, 0),
+      date: new Date(2020, 6, 30, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 6, 30, 11).getTime();
     });
 
     const availability = await listProviderDayhAvailability.execute({
@@ -33,9 +37,12 @@ describe('ListProviderDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 14, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
