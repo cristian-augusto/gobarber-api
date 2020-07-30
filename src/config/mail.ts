b2c76639e50c488afb10567b4c0535241feb3883
Cristian Augusto <cristianaugusto2013@gmail.com>
2020-07-30 12:31:38 -0300

    Mail Config

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@cafecomcodigo.dev',
+      name: 'Cristian - Café com Código',
+    },
+  },
+} as IMailConfig;
